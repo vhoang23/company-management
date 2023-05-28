18765824ca6f4236d6b5f82f5239620add33ec5f
vhoang23 <133740749+vhoang23@users.noreply.github.com>
2023-05-28 20:55:49 +0700

    Update index.js

--- comman-server/src/config/db/index.js
+++ comman-server/src/config/db/index.js
@@ -1,11 +1,1 @@
-const mysql = require("mysql");
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "comdb",
-  charset: "utf8mb4",
-  multipleStatements: true,
-});
 
-module.exports = db;
